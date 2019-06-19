4d35c7de747f88365cdd07c6b3e784788ed0fd9c
wicho1001 <wichoguerrero@yahoo.com>
2019-06-19 13:05:58 -0500

    renaming Session 5

--- Sesion05-GrahQL-1/Ejemplo-01/src/schema/index.js
+++
@@ -1,7 +0,0 @@
-// Import schema definition
-import SchemaDefinition from './schemaDefinition.graphql';
-
-// Import Queries
-import Query from './query.graphql';
-
-export default [SchemaDefinition, Query];
